1c23e0d5d8cbf7dd9baa985ec15d219989fc7a9e
MBGonzalezMiramontes <begonzalezmiramontes@gmail.com>
2024-05-09 19:29:27 -0300

    postgres

--- src/db.js
+++ src/db.js
@@ -5,7 +5,7 @@ const path = require("path");
 const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME } = process.env;
 
 const sequelize = new Sequelize(
-  `mysql://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`,
+  `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`,
   {
     logging: false,
     native: false,
