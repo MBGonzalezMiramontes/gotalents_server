e5d448f84984d97696e9da589c6f249c0432dca2
MBGonzalezMiramontes <begonzalezmiramontes@gmail.com>
2024-05-22 20:21:54 -0300

    cors

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ const morgan = require("morgan");
 const routes = require("./routes/index.js");
 const cors = require("cors");
 const multer = require("multer");
-const path = require('path');
+const path = require("path");
 
 require("./db.js");
 
@@ -19,11 +19,18 @@ server.use(
   })
 );
 server.use(bodyParser.json({ limit: "50mb" }));
+
+// Middleware para el registro de solicitudes
 server.use(morgan("dev"));
-server.use(cors({origin: "https://www.gotalentsglobal.com"}));
+
+// Configuración de CORS
+server.use(cors());
 
 // Ruta absoluta para servir archivos estáticos
-const reactBuildPath = path.join(__dirname, '../../../gotalents_client/gotalents_client/build');
+const reactBuildPath = path.join(
+  __dirname,
+  "../../../gotalents_client/gotalents_client/build"
+);
 server.use(express.static(reactBuildPath));
 
 server.use((req, res, next) => {
@@ -46,9 +53,6 @@ server.options("", (req, res) => {
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.sendStatus(204); // No content in the response
 });
-
-
-
 
 server.use((err, req, res, next) => {
   const status = err.status || 500;
